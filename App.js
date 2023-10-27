4cd2a3500cc0779fd9b8926306ee7bd2634fbbf0
herbeysoft-team <herbeysoft@gmail.com>
2023-10-27 20:47:25 +0100

    First Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppNavigation from './src/navigation'
+
+const App = () => {
+  return (
+    <AppNavigation/>
+  )
+}
+
+export default App
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,126 @@
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import axios from "axios";
+import Categories from "../components/Categories";
+import Recipes from "../components/Recipes";
+
+const HomeScreen = () => {
+  const [activeCategory, setActiveCategory] = useState("Beef");
+  const [categories, setCategories] = useState([]);
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    getCategories();
+    getRecipes();
+  }, []);
+
+  const handleChangeCategory = category=>{
+    getRecipes(category);
+    setActiveCategory(category);
+    setMeals([]);
+  }
+
+  const getCategories = async () => {
+    try {
+      const response = await axios.get(
+        "https://themealdb.com/api/json/v1/1/categories.php"
+      );
+      if (response && response.data) {
+        setCategories(response.data.categories);
+      }
+    } catch (err) {
+      console.log("error: ", err.message);
+    }
+  };
+
+  const getRecipes = async (category="Beef")=>{
+    try{
+      const response = await axios.get(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+      // console.log('got recipes: ',response.data);
+      if(response && response.data){
+        setMeals(response.data.meals);
+      }
+    }catch(err){
+      console.log('error: ',err.message);
+    }
+  }
+
+  return (
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        {/* avatar and bell icon */}
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/images/avatar.png")}
+            style={{ height: hp(5), width: hp(5.5) }}
+          />
+          <BellIcon size={hp(4)} color="gray" />
+        </View>
+
+        {/* greetings and punchline */}
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">
+            Hello, Herbey!
+          </Text>
+          <View>
+            <Text
+              style={{ fontSize: hp(3.8) }}
+              className="font-semibold text-neutral-600"
+            >
+              Make your own food,
+            </Text>
+          </View>
+          <Text
+            style={{ fontSize: hp(3.8) }}
+            className="font-semibold text-neutral-600"
+          >
+            stay at <Text className="text-green-600">home</Text>
+          </Text>
+        </View>
+
+        {/* search bar */}
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput
+            placeholder="Search any recipe"
+            placeholderTextColor={"gray"}
+            style={{ fontSize: hp(1.7) }}
+            className="flex-1 text-base mb-1 pl-3 tracking-wider"
+          />
+          <View className="bg-white rounded-full p-3">
+            <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color="gray" />
+          </View>
+        </View>
+
+        {/* categories */}
+        <View>
+          {categories.length > 0 && (
+            <Categories
+              categories={categories}
+              activeCategory={activeCategory}
+              handleChangeCategory={handleChangeCategory}
+            />
+          )}
+        </View>
+        
+         {/* recipes */}
+         <View>
+          <Recipes meals={meals} categories={categories} />
+        </View>
+
+      </ScrollView>
+    </View>
+  );
+};
+
+export default HomeScreen;
